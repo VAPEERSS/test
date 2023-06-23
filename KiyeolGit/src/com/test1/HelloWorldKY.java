833cfd0b24788c0a4c72dc4d466fb36da0d9a51c
22-ncore-013g <22-ncore-013g@22-ncore-013>
2023-06-23 17:43:38 +0900

    ㅋㅋ

---
+++ KiyeolGit/src/com/test1/HelloWorldKY.java
@@ -0,0 +1,11 @@
+package com.test1;
+
+public class HelloWorldKY {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ji ho 폼 미쳤다이");
+
+	}
+
+}
